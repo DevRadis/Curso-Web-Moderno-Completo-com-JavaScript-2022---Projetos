4ea74f1102be100a2d2e9ceb14f5b0e71e2832a5
Paulo Radis <65192646+DevRadis@users.noreply.github.com>
2023-06-23 20:44:47 -0300

    Aprendendo formas de comentar no JS

---
+++ inicio/comentario.js
@@ -0,0 +1,21 @@
+console.log('Passo #01');
+console.log('Passo #02');
+console.log("Passo #03");
+console.log(4 * 3);
+// console.log(4 + 3);
+
+// Podemos utilizar números e palavras dentro das sentenças, utilizando para palavras como no exemplo acima "" ou ''
+
+/*
+    Esse é um 
+    comentário
+    de múltiplas
+    linhas!
+*/
+
+/*
+ * Esse é um 
+ * comentário de 
+ * múltiplas 
+ * linhas!
+*/
