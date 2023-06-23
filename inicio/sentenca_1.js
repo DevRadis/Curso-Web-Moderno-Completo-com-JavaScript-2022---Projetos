ad9be9745170460fdc775a952672bf3b0a5f8b23
Paulo Radis <65192646+DevRadis@users.noreply.github.com>
2023-06-23 20:32:03 -0300

    Organizando e utilizando sentenças

---
+++ inicio/sentenca_1.js
@@ -0,0 +1,5 @@
+console.log("Bom dia!");
+console.log("Bom tarde!");
+console.log("Bom noite!");
+
+// Podemos colocar mais de uma sentença de código em uma mesma linha. //
---
+++ inicio/sentenca_2.js
@@ -0,0 +1,7 @@
+console.log('Passo #01');
+console.log('Passo #02');
+console.log("Passo #03");
+console.log(4 * 3);
+console.log(4 + 3);
+
+// Podemos utilizar numeros e palavras dentro das sentenças, utilizando para palavras como no exemplo acima "" ou ''
