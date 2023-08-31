827a368929580647dd881f844a9d2ca9572591e6
Paulo Radis <65192646+DevRadis@users.noreply.github.com>
2023-08-31 15:02:10 -0300

    Tipos de dados
    Alguns tipos de dados Number, Boolean, String.

---
+++ inicio/dados_3.js
@@ -0,0 +1,13 @@
+let idade = 31; 
+console.log(typeof 31); //number
+console.log(typeof idade); //Number
+
+let salario = 4578.32;
+console.log(typeof salario); //number
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo); //boolean
+
+console.log(typeof "teste"); //String
+console.log(typeof 'Teste'); //String
+
