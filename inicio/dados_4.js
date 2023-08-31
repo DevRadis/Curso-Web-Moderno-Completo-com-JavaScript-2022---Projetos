5e57465b4919f51e0c848e9f7e4379d469ea0e27
Paulo Radis <65192646+DevRadis@users.noreply.github.com>
2023-08-31 15:25:48 -0300

    Dados + desafio1

---
+++ inicio/dados_4.js
@@ -0,0 +1,9 @@
+const a = 3;
+
+//...
+
+// a = a + 10; // Posso trocar o valor de a usando o =
+
+//...
+
+console.log(a);
---
+++ inicio/desafio_area.js
@@ -0,0 +1,10 @@
+// PI * raio * raio
+
+const raio = 10;
+const PI = 3.141592;
+let areaCirc = PI * raio * raio;
+
+console.log("O resultado da area é " + areaCirc + "M².");
+
+areaCirc = Math.PI * raio * raio;
+console.log("O resultado da area é " + areaCirc + "M².");
